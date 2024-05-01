7a819a499a72ee0691814219b2002d0aa871aee8
Jeevukm <161112690+Jeevukm@users.noreply.github.com>
2024-05-01 11:06:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,10 +9,10 @@ async function main() {
   bot.start(async (ctx) => {
     try {
       ctx.reply(
-        `Hi ${ctx.message.from.first_name},\n\nI can Download Files from Terabox.\n\nMade with ❤️ by @botcodes123\n\nSend any terabox link to download.`,
+        `Hi ${ctx.message.from.first_name},\n\nI can Download Files from Terabox❤️.\n\nMade with 🧑‍💻 by @jeevan_shetty\n\nSend any terabox link to download.`,
         Markup.inlineKeyboard([
-          Markup.button.url(" Channel", "https://t.me/botcodes123"),
-          Markup.button.url("Report bug", "https://t.me/Armanidrisi_bot"),
+          Markup.button.url(" Channel", "https://t.me/shetty_Bots"),
+          Markup.button.url("Report bug", "https://t.me/shettyDiscus_Group"),
         ]),
       );
     } catch (e) {
